a17444c948cc09ad78421c7f8b99184d13a3b8dd
Elli Furedy <elli@failfactory.io>
2025-03-02 19:11:26 -0600

    in the middle of handshake refactor.

--- src/game/quickdraw-states/idle-state.cpp
+++ src/game/quickdraw-states/idle-state.cpp
@@ -89,48 +89,47 @@ void Idle::serialEventCallbacks(string message) {
         player->setOpponentMacAddress(message);
         transitionToHandshakeState = true;
     }
-
 }
 
 
 void Idle::ledAnimation(Device *PDN) {
-    if (breatheUp) {
-        ledBrightness++;
-    } else {
-        ledBrightness--;
-    }
-    pwm_val =
-            255.0 * (1.0 - abs((2.0 * (ledBrightness / smoothingPoints)) - 1.0));
+    // if (breatheUp) {
+    //     ledBrightness++;
+    // } else {
+    //     ledBrightness--;
+    // }
+    // pwm_val =
+    //         255.0 * (1.0 - abs((2.0 * (ledBrightness / smoothingPoints)) - 1.0));
 
-    if (ledBrightness == 255) {
-        breatheUp = false;
-    } else if (ledBrightness == 80) {
-        breatheUp = true;
-    }
+    // if (ledBrightness == 255) {
+    //     breatheUp = false;
+    // } else if (ledBrightness == 80) {
+    //     breatheUp = true;
+    // }
 
-    if (random8() % 8 == 0) {
-        CRGB color = ColorFromPalette(currentPalette, random8(), pwm_val, LINEARBLEND);
-        PDN->addToLight(LightIdentifier::DISPLAY_LIGHTS,
-                        random8() % (numDisplayLights - 1),
-                        LEDColor(color.r, color.g, color.b)
-        );
-    }
+    // if (random8() % 8 == 0) {
+    //     CRGB color = ColorFromPalette(currentPalette, random8(), pwm_val, LINEARBLEND);
+    //     PDN->addToLight(LightIdentifier::DISPLAY_LIGHTS,
+    //                     random8() % (numDisplayLights - 1),
+    //                     LEDColor(color.r, color.g, color.b)
+    //     );
+    // }
 
 
-    for (int i = 0; i < numGripLights; i++) {
-        if (random8() % 130 == 0) {
-            CRGB color = ColorFromPalette(currentPalette, random8(), pwm_val, LINEARBLEND);
-            PDN->addToLight(LightIdentifier::GRIP_LIGHTS,
-                            i,
-                            LEDColor(color.r, color.g, color.b)
-            );
-        }
-    }
+    // for (int i = 0; i < numGripLights; i++) {
+    //     if (random8() % 130 == 0) {
+    //         CRGB color = ColorFromPalette(currentPalette, random8(), pwm_val, LINEARBLEND);
+    //         PDN->addToLight(LightIdentifier::GRIP_LIGHTS,
+    //                         i,
+    //                         LEDColor(color.r, color.g, color.b)
+    //         );
+    //     }
+    // }
 
-    if(random8() % 2) {
-        PDN->fadeLightsBy(LightIdentifier::DISPLAY_LIGHTS, 1);
-        PDN->fadeLightsBy(LightIdentifier::GRIP_LIGHTS, 1);
-    }
+    // if(random8() % 2) {
+    //     PDN->fadeLightsBy(LightIdentifier::DISPLAY_LIGHTS, 1);
+    //     PDN->fadeLightsBy(LightIdentifier::GRIP_LIGHTS, 1);
+    // }
 }
 
 bool Idle::transitionToHandshake() {
