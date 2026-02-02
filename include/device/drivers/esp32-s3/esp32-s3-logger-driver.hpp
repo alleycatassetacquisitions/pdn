6ceeb4dcb3e5254d148b92fbe147cfa62ff3f1a7
Elli Furedy <elli@failfactory.io>
2026-02-01 21:29:59 -0600

    Update logger to output better logs.

--- include/device/drivers/esp32-s3/esp32-s3-logger-driver.hpp
+++ include/device/drivers/esp32-s3/esp32-s3-logger-driver.hpp
@@ -2,10 +2,11 @@
 
 #include "device/drivers/driver-interface.hpp"
 #include <esp_log.h>
+#include <cstring>
 
 /**
  * ESP32-S3 implementation of LoggerInterface.
- * Wraps ESP-IDF's esp_log functions.
+ * Wraps ESP-IDF's esp_log functions with source location tracking.
  */
 class Esp32S3Logger : public LoggerDriverInterface {
 public:
@@ -22,16 +23,32 @@ public:
     void exec() override {
     }
     
-    void vlog(LogLevel level, const char* tag, const char* format, va_list args) override {
+    void vlog(LogLevel level, const char* tag, const char* file, int line, const char* format, va_list args) override {
         char buffer[256];
         vsnprintf(buffer, sizeof(buffer), format, args);
         
+        // Extract just the filename from the full path
+        const char* filename = file;
+        const char* lastSlash = strrchr(file, '/');
+        const char* lastBackslash = strrchr(file, '\\');
+        if (lastSlash) filename = lastSlash + 1;
+        if (lastBackslash && lastBackslash > filename) filename = lastBackslash + 1;
+        
+        // Map our LogLevel to ESP-IDF log level and get level character
+        esp_log_level_t espLevel;
+        char levelChar;
         switch (level) {
-            case LogLevel::ERROR:   ESP_LOGE(tag, "%s", buffer); break;
-            case LogLevel::WARN:    ESP_LOGW(tag, "%s", buffer); break;
-            case LogLevel::INFO:    ESP_LOGI(tag, "%s", buffer); break;
-            case LogLevel::DEBUG:   ESP_LOGD(tag, "%s", buffer); break;
-            case LogLevel::VERBOSE: ESP_LOGV(tag, "%s", buffer); break;
+            case LogLevel::ERROR:   espLevel = ESP_LOG_ERROR;   levelChar = 'E'; break;
+            case LogLevel::WARN:    espLevel = ESP_LOG_WARN;    levelChar = 'W'; break;
+            case LogLevel::INFO:    espLevel = ESP_LOG_INFO;    levelChar = 'I'; break;
+            case LogLevel::DEBUG:   espLevel = ESP_LOG_DEBUG;   levelChar = 'D'; break;
+            case LogLevel::VERBOSE: espLevel = ESP_LOG_VERBOSE; levelChar = 'V'; break;
+            default:                espLevel = ESP_LOG_INFO;    levelChar = '?'; break;
         }
+        
+        // Use esp_log_write with custom format that includes our source location
+        // Format: [timestamp][level][tag][file:line] message
+        esp_log_write(espLevel, tag, "[%6lu][%c][%s][%s:%d] %s\n", 
+                      (unsigned long)(esp_log_timestamp()), levelChar, tag, filename, line, buffer);
     }
 };
--- include/device/drivers/logger.hpp
+++ include/device/drivers/logger.hpp
@@ -22,10 +22,16 @@ public:
     virtual ~LoggerInterface() = default;
     
     /**
-     * Log with va_list.
+     * Log with va_list and source location.
      * This is the core method that implementations must provide.
+     * @param level Log severity level
+     * @param tag Category tag for the log message
+     * @param file Source file name (__FILE__)
+     * @param line Source line number (__LINE__)
+     * @param format Printf-style format string
+     * @param args Variable arguments
      */
-    virtual void vlog(LogLevel level, const char* tag, const char* format, va_list args) = 0;
+    virtual void vlog(LogLevel level, const char* tag, const char* file, int line, const char* format, va_list args) = 0;
 };
 
 /**
@@ -38,11 +44,11 @@ extern LoggerInterface* g_logger;
  * Helper function to wrap variadic arguments and call vlog.
  * This is needed because we can't have variadic virtual functions with multiple inheritance.
  */
-inline void log_helper(LogLevel level, const char* tag, const char* format, ...) {
+inline void log_helper(LogLevel level, const char* tag, const char* file, int line, const char* format, ...) {
     if (g_logger) {
         va_list args;
         va_start(args, format);
-        g_logger->vlog(level, tag, format, args);
+        g_logger->vlog(level, tag, file, line, format, args);
         va_end(args);
     }
 }
@@ -50,18 +56,21 @@ inline void log_helper(LogLevel level, const char* tag, const char* format, ...)
 /**
  * Convenience macros for logging (similar to ESP_LOG style).
  * Usage: LOG_E("TAG", "Error: %d", errorCode);
+ * 
+ * These macros capture __FILE__ and __LINE__ at the call site
+ * so log output shows the actual source location.
  */
 #define LOG_E(tag, format, ...) \
-    log_helper(LogLevel::ERROR, tag, format, ##__VA_ARGS__)
+    log_helper(LogLevel::ERROR, tag, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
 #define LOG_W(tag, format, ...) \
-    log_helper(LogLevel::WARN, tag, format, ##__VA_ARGS__)
+    log_helper(LogLevel::WARN, tag, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
 #define LOG_I(tag, format, ...) \
-    log_helper(LogLevel::INFO, tag, format, ##__VA_ARGS__)
+    log_helper(LogLevel::INFO, tag, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
 #define LOG_D(tag, format, ...) \
-    log_helper(LogLevel::DEBUG, tag, format, ##__VA_ARGS__)
+    log_helper(LogLevel::DEBUG, tag, __FILE__, __LINE__, format, ##__VA_ARGS__)
 
 #define LOG_V(tag, format, ...) \
-    log_helper(LogLevel::VERBOSE, tag, format, ##__VA_ARGS__)
+    log_helper(LogLevel::VERBOSE, tag, __FILE__, __LINE__, format, ##__VA_ARGS__)
