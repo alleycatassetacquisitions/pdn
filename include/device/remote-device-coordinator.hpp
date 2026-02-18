b36e3fca43ecfbe9cfc46b96791bb7956a9cbdcd
Elli Furedy <elli@failfactory.io>
2026-02-18 13:18:27 -0600

    Flesh out the RemoteDeviceCoordinator header file.

--- include/device/remote-device-coordinator.hpp
+++ include/device/remote-device-coordinator.hpp
@@ -1,15 +1,32 @@
 #pragma once
 
-#include "device/drivers/peer-comms-interface.hpp"
-#include "device/drivers/serial-wrapper.hpp"
+#include "device/serial-manager.hpp"
+#include "wireless/remote-player-manager.hpp"
+#include "apps/handshake/handshake.hpp"
+
+enum class PortStatus {
+    DISCONNECTED = 0,
+    CONNECTED = 1,
+    DAISY_CHAINED = 2, //Not implementing this yet.
+};
+
+struct PortState {
+    SerialByState port;
+    PortStatus status;
+    RemotePlayer* peerList;
+};
 
 class RemoteDeviceCoordinator {
 public:
     RemoteDeviceCoordinator();
 
-    void initialize(PeerCommsInterface* peerComms, HWSerialWrapper* serial1, HWSerialWrapper* serial2);
+    void initialize(RemotePlayerManager* remotePlayerManager, SerialManager* serialManager);
 
     void sync();
+
+    PortStatus getPortStatus(SerialByState port);
+
+    PortState getPortState(SerialByState port);
 
 private:
 
@@ -17,10 +34,27 @@ private:
     bool checkForHeartbeat();
     void notifyDisconnect();
     void notifyConnect();
+    void notifyDaisyChained();
 
-    void manageConnectedPeers();
+    PortStatus mapHandshakeStateToStatus(SerialByState port);
 
-    PeerCommsInterface* peerComms;
-    HWSerialWrapper* serial1;
-    HWSerialWrapper* serial2;
-}
+    void addDaisyChainedPeer(RemotePlayer remotePlayer);
+    void removeDaisyChainedPeer(RemotePlayer remotePlayer);
+
+    void registerPeer(RemotePlayer remotePlayer);
+    void unregisterPeer(RemotePlayer remotePlayer);
+
+    RemotePlayerManager* remotePlayerManager;
+    SerialManager* serialManager;
+
+    HandshakeApp primaryPortHandshake;
+    HandshakeApp auxPortHandshake;
+
+    SimpleTimer heartbeatTimer;
+    SimpleTimer timeout;
+
+    const int HEARTBEAT_INTERVAL = 250;
+    const int TIMEOUT_INTERVAL = 700;
+
+    std::function<void()> onPrimaryPortDisconnected;
+};
